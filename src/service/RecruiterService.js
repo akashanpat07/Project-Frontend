20e68edf9ef9bba1864f82b6a519de9b76d78f5f
Akash Anpat <54320309+akashanpat07@users.noreply.github.com>
2024-08-28 18:41:44 +0530

    Update RecruiterService.js

--- src/service/RecruiterService.js
+++ src/service/RecruiterService.js
@@ -1,83 +1,85 @@
-import axios from "axios"
-import { useDispatch } from "react-redux"
+import axios from "axios";
+import { useDispatch } from "react-redux";
 import { setRecruiterDetails } from "../redux/slices/Recruiter/RecruiterSlice";
+
+// Base URL for the API, replace with your domain
+const API_BASE_URL = "http://jobnexus"; // or https://jobnexus if using HTTPS
 
 class RecruiterService {
 
-    //To get JWT Token from Backend
+    // To get JWT Token from Backend
     authenticateRecruiter(recruiter) {
-        return axios.post("http://localhost:8080/recruiter/authenticate", recruiter)
+        return axios.post(`${API_BASE_URL}/recruiter/authenticate`, recruiter);
     }
 
-    //Get Recruiter Details from Backend using JWT Token
+    // Get Recruiter Details from Backend using JWT Token
     loadUserByJwtToken(jwtToken) {
-        return axios.get("http://localhost:8080/recruiter", {
+        return axios.get(`${API_BASE_URL}/recruiter`, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 
-    //Post a Job, by Recruiter JWT Token
+    // Post a Job, by Recruiter JWT Token
     createNewJob(job, jwtToken) {
-        return axios.post("http://localhost:8080/jobs/job", job, {
+        return axios.post(`${API_BASE_URL}/jobs/job`, job, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 
-    //To Get Applicants Datas for particular Job id
+    // To Get Applicants Data for particular Job id
     getApplicantsOfJob(id, jwtToken) {
-        return axios.get("http://localhost:8080/jobs/applicants/" + id, {
+        return axios.get(`${API_BASE_URL}/jobs/applicants/${id}`, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 
     getResumeOfApplicant(email, jwtToken) {
-        return axios.get('http://localhost:8080/jobseeker/resume/' + email, {
+        return axios.get(`${API_BASE_URL}/jobseeker/resume/${email}`, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 
     hireApplicant(jobApplicationId, jwtToken) {
-        return axios.get('http://localhost:8080/job-application/hire/'+jobApplicationId, {
+        return axios.get(`${API_BASE_URL}/job-application/hire/${jobApplicationId}`, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 
     updateApplicationStatus(jobApplicationId, status, jwtToken) {
-        return axios.get('http://localhost:8080/job-application/update/'+status+"/"+jobApplicationId, {
+        return axios.get(`${API_BASE_URL}/job-application/update/${status}/${jobApplicationId}`, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 
-    //Register new Recruiter
+    // Register new Recruiter
     registerRecruiter(recruiter, formdata) {
-        return axios.post("http://localhost:8080/recruiter", recruiter)
-
+        return axios.post(`${API_BASE_URL}/recruiter`, recruiter);
     }
 
-    //Upload Company Logo
+    // Upload Company Logo
     uploadCompanyLogo(registeredRecruiterID, formdata) {
-        return axios.post(`http://localhost:8080/recruiter/image/${registeredRecruiterID}`, formdata)
+        return axios.post(`${API_BASE_URL}/recruiter/image/${registeredRecruiterID}`, formdata);
     }
 
-    //Delete a Job by ID with valid JWT Token
+    // Delete a Job by ID with valid JWT Token
     deleteJob(id, jwtToken) {
-        return axios.delete("http://localhost:8080/jobs/job/" + id, {
+        return axios.delete(`${API_BASE_URL}/jobs/job/${id}`, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
-        })
+        });
     }
 }
 
