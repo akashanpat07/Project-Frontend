77571fcb420c038d3c5bad8c999066f619b5a19d
Akash Anpat <54320309+akashanpat07@users.noreply.github.com>
2024-09-05 00:07:44 +0530

    Update RecruiterService.js

--- src/service/RecruiterService.js
+++ src/service/RecruiterService.js
@@ -1,130 +1,83 @@
-import axios from "axios";
-
-// Automatically use the current protocol (HTTP or HTTPS)
-const API_BASE_URL = `https://www.jobnexus.in`;
+import axios from "axios"
+import { useDispatch } from "react-redux"
+import { setRecruiterDetails } from "../redux/slices/Recruiter/RecruiterSlice";
 
 class RecruiterService {
-    // To get JWT Token from Backend
+
+    //To get JWT Token from Backend
     authenticateRecruiter(recruiter) {
-        return axios.post(`${API_BASE_URL}/recruiter/authenticate`, recruiter)
-            .catch(error => {
-                // Handle errors here
-                console.error("Error authenticating recruiter:", error);
-                throw error;
-            });
+        return axios.post("https://www.jobnexus.in/recruiter/authenticate", recruiter)
     }
 
-    // Get Recruiter Details from Backend using JWT Token
+    //Get Recruiter Details from Backend using JWT Token
     loadUserByJwtToken(jwtToken) {
-        return axios.get(`${API_BASE_URL}/recruiter`, {
+        return axios.get("https://www.jobnexus.in/recruiter", {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error loading user by JWT token:", error);
-            throw error;
-        });
     }
 
-    // Post a Job, by Recruiter JWT Token
+    //Post a Job, by Recruiter JWT Token
     createNewJob(job, jwtToken) {
-        return axios.post(`${API_BASE_URL}/jobs/job`, job, {
+        return axios.post("https://www.jobnexus.in/jobs/job", job, {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error creating new job:", error);
-            throw error;
-        });
     }
 
-    // To Get Applicants Data for particular Job id
+    //To Get Applicants Datas for particular Job id
     getApplicantsOfJob(id, jwtToken) {
-        return axios.get(`${API_BASE_URL}/jobs/applicants/${id}`, {
+        return axios.get("https://www.jobnexus.in/jobs/applicants/" + id, {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error getting applicants of job:", error);
-            throw error;
-        });
     }
 
-    // Get Resume of Applicant
     getResumeOfApplicant(email, jwtToken) {
-        return axios.get(`${API_BASE_URL}/jobseeker/resume/${email}`, {
+        return axios.get('https://www.jobnexus.in/jobseeker/resume/' + email, {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error getting resume of applicant:", error);
-            throw error;
-        });
     }
 
-    // Hire Applicant
     hireApplicant(jobApplicationId, jwtToken) {
-        return axios.get(`${API_BASE_URL}/job-application/hire/${jobApplicationId}`, {
+        return axios.get('https://www.jobnexus.in/job-application/hire/'+jobApplicationId, {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error hiring applicant:", error);
-            throw error;
-        });
     }
 
-    // Update Application Status
     updateApplicationStatus(jobApplicationId, status, jwtToken) {
-        return axios.get(`${API_BASE_URL}/job-application/update/${status}/${jobApplicationId}`, {
+        return axios.get('https://www.jobnexus.in/job-application/update/'+status+"/"+jobApplicationId, {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error updating application status:", error);
-            throw error;
-        });
     }
 
-    // Register new Recruiter
-    registerRecruiter(recruiter) {
-        return axios.post(`${API_BASE_URL}/recruiter`, recruiter)
-            .catch(error => {
-                console.error("Error registering recruiter:", error);
-                throw error;
-            });
+    //Register new Recruiter
+    registerRecruiter(recruiter, formdata) {
+        return axios.post("https://www.jobnexus.in/recruiter", recruiter)
+
     }
 
-    // Upload Company Logo
-    uploadCompanyLogo(registeredRecruiterID, formData) {
-        return axios.post(`${API_BASE_URL}/recruiter/image/${registeredRecruiterID}`, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
-        .catch(error => {
-            console.error("Error uploading company logo:", error);
-            throw error;
-        });
+    //Upload Company Logo
+    uploadCompanyLogo(registeredRecruiterID, formdata) {
+        return axios.post(`https://www.jobnexus.in/recruiter/image/${registeredRecruiterID}`, formdata)
     }
 
-    // Delete a Job by ID with valid JWT Token
+    //Delete a Job by ID with valid JWT Token
     deleteJob(id, jwtToken) {
-        return axios.delete(`${API_BASE_URL}/jobs/job/${id}`, {
+        return axios.delete("https://www.jobnexus.in/jobs/job/" + id, {
             headers: {
-                "Authorization": `Bearer ${jwtToken}`,
+                "Authorization": "Bearer " + jwtToken,
             }
         })
-        .catch(error => {
-            console.error("Error deleting job:", error);
-            throw error;
-        });
     }
 }
 
