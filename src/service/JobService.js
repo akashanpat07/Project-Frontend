7e6a30d58f0cc41cb3f49b4250bba4034bbaa309
Akash Anpat <54320309+akashanpat07@users.noreply.github.com>
2024-09-05 00:06:00 +0530

    Update JobService.js

--- src/service/JobService.js
+++ src/service/JobService.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 
-// Automatically use the current protocol (HTTP or HTTPS)
-const API_BASE_URL = `https://www.jobnexus.in`;
-
 class JobService {
 
-    getAllJobs() {
-        return axios.get(`${API_BASE_URL}/jobs`);
+    getAllJobs(){
+        return axios.get("https://www.jobnexus.in/jobs");
     }
 
-    applyForJob(id, jwtToken) {
-        return axios.get(`${API_BASE_URL}/job-application/${id}`, {
-            headers: {
+    applyForJob(id, jwtToken)
+    {
+        return axios.get("https://www.jobnexus.in/job-application/"+id,{
+            headers : {
                 "Authorization": "Bearer " + jwtToken
             }
-        });
+        })
     }
 
 }
