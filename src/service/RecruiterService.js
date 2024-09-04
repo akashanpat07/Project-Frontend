e4d5565e89d8f97c7b42b77d2b7aa1a6f2ae79a1
Akash Anpat <54320309+akashanpat07@users.noreply.github.com>
2024-09-05 01:41:03 +0530

    Update RecruiterService.js

--- src/service/RecruiterService.js
+++ src/service/RecruiterService.js
@@ -6,12 +6,12 @@ class RecruiterService {
 
     //To get JWT Token from Backend
     authenticateRecruiter(recruiter) {
-        return axios.post("https://www.jobnexus.in/recruiter/authenticate", recruiter)
+        return axios.post("https://api.jobnexus.in/recruiter/authenticate", recruiter)
     }
 
     //Get Recruiter Details from Backend using JWT Token
     loadUserByJwtToken(jwtToken) {
-        return axios.get("https://www.jobnexus.in/recruiter", {
+        return axios.get("https://api.jobnexus.in/recruiter", {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
@@ -20,7 +20,7 @@ class RecruiterService {
 
     //Post a Job, by Recruiter JWT Token
     createNewJob(job, jwtToken) {
-        return axios.post("https://www.jobnexus.in/jobs/job", job, {
+        return axios.post("https://api.jobnexus.in/jobs/job", job, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
@@ -29,7 +29,7 @@ class RecruiterService {
 
     //To Get Applicants Datas for particular Job id
     getApplicantsOfJob(id, jwtToken) {
-        return axios.get("https://www.jobnexus.in/jobs/applicants/" + id, {
+        return axios.get("https://api.jobnexus.in/jobs/applicants/" + id, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
@@ -37,7 +37,7 @@ class RecruiterService {
     }
 
     getResumeOfApplicant(email, jwtToken) {
-        return axios.get('https://www.jobnexus.in/jobseeker/resume/' + email, {
+        return axios.get('https://api.jobnexus.in/jobseeker/resume/' + email, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
@@ -45,7 +45,7 @@ class RecruiterService {
     }
 
     hireApplicant(jobApplicationId, jwtToken) {
-        return axios.get('https://www.jobnexus.in/job-application/hire/'+jobApplicationId, {
+        return axios.get('https://api.jobnexus.in/job-application/hire/'+jobApplicationId, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
@@ -53,7 +53,7 @@ class RecruiterService {
     }
 
     updateApplicationStatus(jobApplicationId, status, jwtToken) {
-        return axios.get('https://www.jobnexus.in/job-application/update/'+status+"/"+jobApplicationId, {
+        return axios.get('https://api.jobnexus.in/job-application/update/'+status+"/"+jobApplicationId, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
@@ -62,18 +62,18 @@ class RecruiterService {
 
     //Register new Recruiter
     registerRecruiter(recruiter, formdata) {
-        return axios.post("https://www.jobnexus.in/recruiter", recruiter)
+        return axios.post("https://api.jobnexus.in/recruiter", recruiter)
 
     }
 
     //Upload Company Logo
     uploadCompanyLogo(registeredRecruiterID, formdata) {
-        return axios.post(`https://www.jobnexus.in/recruiter/image/${registeredRecruiterID}`, formdata)
+        return axios.post(`https://api.jobnexus.in/recruiter/image/${registeredRecruiterID}`, formdata)
     }
 
     //Delete a Job by ID with valid JWT Token
     deleteJob(id, jwtToken) {
-        return axios.delete("https://www.jobnexus.in/jobs/job/" + id, {
+        return axios.delete("https://api.jobnexus.in/jobs/job/" + id, {
             headers: {
                 "Authorization": "Bearer " + jwtToken,
             }
